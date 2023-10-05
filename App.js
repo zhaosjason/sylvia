57f6f5688e9634c5427afd7c10f2958170d854d3
Jason Zhao <jason@zhaobot.mynetworksettings.com>
2023-10-05 18:17:41 -0400

    Welcome page tweaks

--- App.js
+++ App.js
@@ -6,16 +6,15 @@ export default function App() {
     <View className="wrapper" style={styles.wrapper}>
       <View className="container_door" style={styles.container_door}>
         <View className="door" style={styles.door} />
-        {/* <View className="Rectangle35" style={{ width: 435.50, height: 899, left: -15.50, top: -47, position: 'absolute', backgroundColor: 'radial-gradient(90.02% 121.67% at 217.61% 524.22%, #FFB997 0%, rgba(117, 139, 253, 0) 100%)' }}></View> */}
       </View>
       <View className="container_text" style={styles.container_text}>
-        <View className="Sylvia" style={{textAlign: 'center', color: 'white', fontSize: 40, fontWeight: '400', lineHeight: 60, wordWrap: 'break-word' }}>
-          <Text style={{color: 'white', fontSize: 40, fontWeight: '400'}}>Sylvia</Text>
+        <View className="container_title" style={styles.container_title}>
+          <Text style={styles.title}>Sylvia</Text>
+          <Logo style={styles.logo} width={60} height={60} />
         </View>
-        {/* <View className="HelloWelcomeToSylvia" style={{ width: 242, height: 61, left: 86, top: 436, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 20, fontWeight: '400', lineHeight: 30, wordWrap: 'break-word' }}>
-          <Text>Hello, Welcome to Sylvia.</Text>
-        </View> */}
-        <Logo width={120} height={40} />
+        <View className="container_subtitle" style={styles.container_subtitle}>
+          <Text style={styles.subtitle}>Hello, Welcome to Sylvia.</Text>
+        </View>
       </View>
     </View>
   );
@@ -37,10 +36,24 @@ const styles = StyleSheet.create({
 
   door: {
     width: '20%',
-    height: '33%',
+    height: '40%',
     top: '33%',
     left: '50%',
     backgroundColor: '#FFB997',
+  },
+
+  door_light: {
+    // width: 435.50, 
+    // height: 899, 
+    // left: -15.50, 
+    // top: -47, 
+    width: '20%',
+    height: '40%',
+    top: '50%',
+    left: '50%',
+    position: 'absolute', 
+    backgroundColor: 'radial-gradient(90.02% 121.67% at 217.61% 524.22%, #FFB997 0%, rgba(117, 139, 253, 0) 100%)',
+    // backgroundColor: 'red',
   },
 
   container_text: {
@@ -48,5 +61,36 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
     position: 'absolute',
+  },
+
+  container_title: {
+    top: '65%',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    // borderWidth: 1,
+    // borderColor: "red",
+  },
+
+  title: {
+    color: 'white', 
+    fontSize: 40, 
+    fontWeight: '400',
+  },
+
+  logo: {
+    marginBottom: 5,
+  },
+
+  container_subtitle: {
+    top: '72%',
+    flexDirection: 'row',
+    textAlign: 'center',
+    wordWrap: 'break-word',
+  },
+
+  subtitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '300',
   },
 });
